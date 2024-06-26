595f556c3318474afb8d670b010b12f1b572cd80
Irakli <112602982+IkaMastera@users.noreply.github.com>
2024-06-26 17:53:32 +0400

    add: add alarm functionality

--- script.js
+++ script.js
@@ -58,6 +58,55 @@ const InputCheck = (inputValue) => {
   return inputValue;
 };
 
+hourInput.addEventListener("input", () => {
+  hourInput.value = InputCheck(hourInput.value);
+});
+
+minuteInput.addEventListener("input", () => {
+  minuteInput.value = InputCheck(minuteInput.value);
+});
+
+const createAlarm = (alarmObj) => {
+  const { id, alarmHour, alarmMinute } = alarmObj;
+
+  let alarmDiv = document.createElement("div");
+  alarmDiv.classList.add("alarm");
+  alarmDiv.setAttribute("data-id", id);
+  alarmDiv.innerHTML = `<span>${alarmHour}: ${alarmMinute}</span>`;
+
+  let checkbox = document.createElement("input");
+  checkbox.setAttribute("type", "checkbox");
+  checkbox.addEventListener("click", (e) => {
+    if (e.target.checked) {
+      startAlarm(e);
+    } else {
+      stopAlarm(e);
+    }
+  });
+
+  alarmDiv.appendChild(checkbox);
+  let deleteButton = document.createElement("button");
+  deleteButton.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
+  deleteButton.classList.add("deleteButton");
+  deleteButton.addEventListener("click", (e) => deleteAlarm(e));
+  alarmDiv.appendChild(deleteButton);
+  activeAlarms.appendChild(alarmDiv);
+};
+
+setAlarm.addEventListener("click", () => {
+  alarmIndex += 1;
+
+  let alarmObj = [];
+  alarmObj.id = `${alarmIndex}_${hourInput.value}_${minuteInput.value}`;
+  alarmObj.alarmHour = hourInput.value;
+  alarmObj.alarmMinute = minuteInput.value;
+  alarmObj.isActive = false;
+  alarmsArray.push(alarmObj);
+  createAlarm(alarmObj);
+  hourInput.value = appendZero(initialHour);
+  minuteInput.value = appendZero(initialMinute);
+});
+
 window.onload = () => {
   setInterval(displayTimer);
   initialHour = 0;
